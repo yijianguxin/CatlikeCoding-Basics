cb92118feb58e5ed1c553f6110eca0f55b55e9ce
yijianguxin <yijianguxin626@gmail.com>
2024-08-19 21:19:56 +0800

    学习第三节课，新增了一些函数类型

--- Assets/Scritps/Graph.cs
+++ Assets/Scritps/Graph.cs
@@ -10,19 +10,19 @@ public class Graph : MonoBehaviour
     [SerializeField, Range(10, 100)]
     private int resolution = 10;
 
+    [SerializeField]
+    private FunctionLibrary.FunctionName function;
+
     private Transform[] points;
 
     private void Awake()
     {
         float step = 2f / resolution;
-        Vector3 position = Vector3.zero;
         var scale = Vector3.one * step;
-        points = new Transform[resolution];
+        points = new Transform[resolution * resolution];
         for (int i = 0; i < points.Length; i++)
         {
             var point = points[i] = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step - 1f;
-            point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
         }
@@ -30,13 +30,20 @@ public class Graph : MonoBehaviour
 
     private void Update()
     {
+        var f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
-        for (int i = 0; i < points.Length; i++)
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
-            var point = points[i];
-            Vector3 position = point.localPosition;
-            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
-            point.localPosition = position;
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
         }
     }
 }
