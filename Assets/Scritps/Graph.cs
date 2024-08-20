9b3f68f5538e9c976d7f8fbdb3fdb40b16cb30b4
yijianguxin <yijianguxin@foxmail.com>
2024-08-20 22:05:21 +0800

    学习了性能测试的一些工具，以及动画渐变

--- Assets/Scritps/Graph.cs
+++ Assets/Scritps/Graph.cs
@@ -13,7 +13,18 @@ public class Graph : MonoBehaviour
     [SerializeField]
     private FunctionLibrary.FunctionName function;
 
+    public enum TransitionMode { Cycle, Random }
+
+    [SerializeField]
+    private TransitionMode transitionMode;
+
+    [SerializeField]
+    private float functionDuration = 1f, transitionDuration = 1f;
+
     private Transform[] points;
+    private float duration;
+    private bool transitioning;
+    private FunctionLibrary.FunctionName transitionFunction;
 
     private void Awake()
     {
@@ -29,6 +40,64 @@ public class Graph : MonoBehaviour
     }
 
     private void Update()
+    {
+        duration += Time.deltaTime;
+        if (transitioning)
+        {
+            if (duration >= transitionDuration)
+            {
+                duration -= transitionDuration;
+                transitioning = false;
+            }
+        }
+        else if (duration >= functionDuration)
+        {
+            duration -= functionDuration;
+            transitioning = true;
+            transitionFunction = function;
+            this.PickNextFunction();
+        }
+
+        if (transitioning)
+        {
+            UpdateFunctionTransition();
+        }
+        else
+        {
+            this.UpdateFunction();
+        }
+    }
+
+    private void PickNextFunction()
+    {
+        function = transitionMode == TransitionMode.Cycle ?
+            FunctionLibrary.GetNextFunctionName(function) :
+            FunctionLibrary.GetRandomFunctionNameOtherThan(function);
+    }
+    
+    private void UpdateFunctionTransition()
+    {
+        FunctionLibrary.Function 
+            from = FunctionLibrary.GetFunction(transitionFunction),
+            to = FunctionLibrary.GetFunction(function);
+        float progress = duration / transitionDuration;
+        float time = Time.time;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = FunctionLibrary.Morph(u, v, time, from, to, progress);
+        }
+    }
+
+    private void UpdateFunction()
     {
         var f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
